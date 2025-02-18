05797a514911e424d02650c28e00b404a0e130f7
radekTomczyk <84013365+radektomczyk@users.noreply.github.com>
2025-02-18 19:02:11 +0100

    Delete front directory

--- front/src/components/Users/User.jsx
+++
@@ -1,8 +0,0 @@
-const User = (props) => {
-    return(
-        <tr><th></th><td style={{paddingRight:'10px'}}>{props.user.firstName}</td><td style={{paddingLeft:'10px'}}>{props.user.lastName}</td></tr>
-    ) 
-    
-}
-
-export default User
